608911b82b8a99064b0dfef0fc7bc97558b66c3e
Eric Loges Binsfeld <ekilobin@gmail.com>
2021-12-12 08:28:29 -0300

    [ADD] Day 11

---
+++ Day11.2.py
@@ -0,0 +1,41 @@
+from inputs.input import get_input
+
+inp = [[int(j) for j in i] for i in get_input(11).read().split("\n")]
+
+flashes = 0
+
+def flash(y, x):
+    if (inp[y][x] == None or inp[y][x] <= 9):
+        return
+    inp[y][x] = None
+    global flashes
+    flashes += 1
+    for i in range(y-1, y+2):
+        for j in range(x-1, x+2):
+            if (0 > i or i > len(inp[0])-1 or 0 > j or j > len(inp[0])-1):
+                continue
+            if (inp[i][j] == None):
+                continue
+            inp[i][j] += 1
+            flash(i, j)
+
+def step():
+    for line in inp:
+        for i in range(len(line)):
+            line[i] += 1
+    
+    for y in range(len(inp)):
+        for x in range(len(inp[y])):
+            flash(y, x)
+    
+    for i in inp:
+        for j in range(len(i)):
+            if i[j] == None:
+                i[j] = 0
+
+steps = 0
+while (flashes != (len(inp)*len(inp[1]))):
+    flashes = 0
+    steps += 1
+    step()
+print(steps)
